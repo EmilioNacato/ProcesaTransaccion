540d2a4b4f76db9a0a5c03f0ce96153ed00afcf2
Anthony Quishpe <aequishpe2@espe.edu.ec>
2025-02-24 12:13:49 -0500

    Refactorizar Transaccion

--- src/main/java/com/banquito/paymentprocessor/procesatransaccion/banquito/controller/HistorialEstadoTransaccionController.java
+++ src/main/java/com/banquito/paymentprocessor/procesatransaccion/banquito/controller/HistorialEstadoTransaccionController.java
@@ -1,7 +1,13 @@
 package com.banquito.paymentprocessor.procesatransaccion.banquito.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.banquito.paymentprocessor.procesatransaccion.banquito.controller.dto.HistorialEstadoTransaccionDTO;
 import com.banquito.paymentprocessor.procesatransaccion.banquito.controller.mapper.HistorialEstadoTransaccionMapper;
@@ -9,8 +15,8 @@ import com.banquito.paymentprocessor.procesatransaccion.banquito.model.Historial
 import com.banquito.paymentprocessor.procesatransaccion.banquito.service.HistorialEstadoTransaccionService;
 
 import jakarta.validation.Valid;
-import lombok.extern.slf4j.Slf4j;
 
+import lombok.extern.slf4j.Slf4j;
 import java.util.List;
 import java.util.stream.Collectors;
 
